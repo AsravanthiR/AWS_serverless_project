def35b3f8d1ca65ffe863d54538721012cd7c0e7
sravanthi <sravanthi.avadanula@gmail.com>
2022-08-04 11:34:07 +0300

    Final version of the project

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let endpoint =
   "https://w6r8w9lldk.execute-api.us-east-1.amazonaws.com/deploy/kanye";
 
-let imageElement = document.querySelector("#unsplashImage");
+let imageElement = document.querySelector("#imageDetails");
 
-let kanyeQuote = document.querySelector(".imageDetails");
+let kanyeQuote = document.querySelector(".quoteDetails");
 
 fetch(endpoint)
   .then(function (response) {
